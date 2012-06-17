86c204fd395c2092619873b6758e9899b43a745b
William Thiago <williamthiago@gmail.com>
2012-06-16 20:08:24 -0400

    Criando dominio

---
+++ MaxPerson.Dominio.Teste/ContaTeste.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MaxPerson.Dominio.Teste
+{
+    [TestClass]
+    public class ContaTeste
+    {
+        [TestMethod]
+        public void Adicionar_Credito_Em_Conta()
+        {
+            // Arrange
+            Conta conta = new Conta();
+
+            // Act
+            conta.Credito.Acrescentar(10);
+
+            // Assert
+            Assert.AreEqual(conta.Credito.CreditoDisponivel, 10);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Adicionar_Credito_Negativo_Em_Conta()
+        {
+            // Arrange
+            Conta conta = new Conta();
+
+            // Act
+            conta.Credito.Acrescentar(-10);
+        }
+
+        [TestMethod]
+        public void Remover_Credito_Em_Conta()
+        {
+            // Arrange
+            Conta conta = new Conta();
+            conta.Credito.Acrescentar(10);
+
+            // Act
+            conta.Credito.Remover(5);
+
+            // Assert
+            Assert.AreEqual(conta.Credito.CreditoDisponivel, 5);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Remover_Credito_Negativo_Em_Conta()
+        {
+            // Arrange
+            Conta conta = new Conta();
+
+            // Act
+            conta.Credito.Remover(-10);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Remover_Credito_A_Mais_Em_Conta()
+        {
+            // Arrange
+            Conta conta = new Conta();
+            conta.Credito.Acrescentar(10);
+
+            // Act
+            conta.Credito.Remover(20);
+        }
+
+        [TestMethod]
+        public void Reservar_Credito_Em_Conta()
+        {
+            // Arrange
+            Conta conta = new Conta();
+            conta.Credito.Acrescentar(10);
+
+            // Act
+            conta.Credito.Reservar(3);
+
+            // Assert
+            Assert.AreEqual(conta.Credito.CreditoDisponivel, 7);
+            Assert.AreEqual(conta.Credito.CreditoReservado, 3);
+            Assert.AreEqual(conta.Credito.Saldo, 10);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Reservar_Credito_Negativo_Em_Conta()
+        {
+            // Arrange
+            Conta conta = new Conta();
+            conta.Credito.Acrescentar(10);
+
+            // Act
+            conta.Credito.Reservar(-3);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Reservar_Credito_A_Mais_Em_Conta()
+        {
+            // Arrange
+            Conta conta = new Conta();
+            conta.Credito.Acrescentar(10);
+
+            // Act
+            conta.Credito.Reservar(20);
+        }
+
+        [TestMethod]
+        public void Cancelar_Reserva_Em_Conta()
+        {
+            // Arrange
+            Conta conta = new Conta();
+            conta.Credito.Acrescentar(10);
+            conta.Credito.Reservar(3);
+
+            // Act
+            conta.Credito.CancelarReserva(3);
+
+            // Assert
+            Assert.AreEqual(conta.Credito.CreditoDisponivel, 10);
+            Assert.AreEqual(conta.Credito.CreditoReservado, 0);
+            Assert.AreEqual(conta.Credito.Saldo, 10);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Cancelar_Reserva_Negativo_Em_Conta()
+        {
+            // Arrange
+            Conta conta = new Conta();
+            conta.Credito.Acrescentar(10);
+            conta.Credito.Reservar(3);
+
+            // Act
+            conta.Credito.CancelarReserva(-3);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Cancelar_Reserva_A_Mais_Em_Conta()
+        {
+            // Arrange
+            Conta conta = new Conta();
+            conta.Credito.Acrescentar(10);
+            conta.Credito.Reservar(5);
+
+            // Act
+            conta.Credito.CancelarReserva(6);
+        }
+
+    }
+}
---
+++ MaxPerson.Dominio/Conta.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxPerson.Dominio
+{
+    public class Conta
+    {
+        public int ID { get; set; }
+
+        public Credito Credito { get; set; }
+
+        public Conta()
+        {
+            this.Credito = new Credito();
+        }
+    }
+
+    public class Credito
+    {
+        public long CreditoDisponivel { get; set; }
+        public long CreditoReservado { get; set; }
+
+        public long Saldo
+        {
+            get
+            {
+                return CreditoDisponivel + CreditoReservado;
+            }
+        }
+
+        public void Reservar(long valor)
+        {
+            if (valor < 0)
+                throw new ArgumentOutOfRangeException("valor", "O valor deve ser maior que zero.");
+
+            if (valor > CreditoDisponivel)
+                throw new ArgumentOutOfRangeException("valor", "O valor deve ser menor que o saldo atual.");
+
+            this.CreditoReservado += valor;
+            this.CreditoDisponivel -= valor;
+        }
+
+        public void CancelarReserva(long valor)
+        {
+            if (valor < 0)
+                throw new ArgumentOutOfRangeException("valor", "O valor deve ser maior que zero.");
+
+            if (valor > CreditoReservado)
+                throw new ArgumentOutOfRangeException("valor", "O valor deve ser menor que o saldo reservado atual.");
+
+            this.CreditoReservado -= valor;
+            this.CreditoDisponivel += valor;
+        }
+
+        public void Acrescentar(long valor)
+        {
+            if (valor < 0)
+                throw new ArgumentOutOfRangeException("valor", "O valor deve ser maior que zero.");
+
+            this.CreditoDisponivel += valor;
+        }
+
+        public void Remover(long valor)
+        {
+            if (valor < 0)
+                throw new ArgumentOutOfRangeException("valor", "O valor deve ser maior que zero.");
+
+            if (valor > CreditoDisponivel)
+                throw new ArgumentOutOfRangeException("valor", "O valor deve ser menor que o saldo atual.");
+
+            this.CreditoDisponivel -= valor;
+        }
+    }
+}
